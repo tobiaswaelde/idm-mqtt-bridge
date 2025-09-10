7ad57deb069304639b0343aa14407598b8e9f6c8
Tobias Wälde <tobias.waelde@gmail.com>
2025-09-10 11:16:21 +0200

    implement bridge

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log('Hello, IDM-MQTT Bridge!');
+import { ENV } from './config/env';
+import { logger } from './config/logger';
+import { TOPICS } from './config/topics';
+import { MyIdm } from './services/idm';
+import { mqttClient } from './services/mqtt';
+import { IdmCommand, StatsType } from './types/idm-cmd';
+import { getJsonPaths } from './util/json';
+
+function publish(topic: string, data: any) {
+	if (ENV.PUSH_JSON_OBJECT) {
+		mqttClient.publish(topic, JSON.stringify(data));
+	}
+
+	if (ENV.PUSH_JSON_KEYS) {
+		const paths = getJsonPaths(data, '/');
+		for (const [path, value] of paths) {
+			mqttClient.publish(`${topic}/${path}`, value);
+		}
+	}
+}
+
+const idm = new MyIdm(ENV.IDM_HOST, ENV.IDM_PIN);
+
+//#region IDM to MQTT
+idm.on('connected', () => {
+	mqttClient.publish(TOPICS.IDM.ONLINE, 'true');
+});
+idm.on('disconnected', () => {
+	mqttClient.publish(TOPICS.IDM.ONLINE, 'false');
+});
+idm.on('info', (data) => {
+	publish(TOPICS.IDM.INFO, data);
+});
+idm.on('heatpump', (data) => {
+	publish(TOPICS.IDM.HEATPUMP, data);
+});
+idm.on('stats', ({ type, data }) => {
+	switch (type) {
+		case StatsType.Heatpump:
+			publish(TOPICS.IDM.STATS.HEATPUMP, data);
+			break;
+		case StatsType.AmountOfHeat:
+			publish(TOPICS.IDM.STATS.AMOUNTOFHEAT, data);
+			break;
+		case StatsType.BaEnergyHp:
+			publish(TOPICS.IDM.STATS.BAENERGYHP, data);
+			break;
+	}
+});
+//#endregion
+
+//#region MQTT to IDM
+mqttClient.on('message', async (topic, payload) => {
+	const msg = payload.toString();
+	if (!msg || msg === '') return;
+	if (topic !== TOPICS.MQTT.CMD) return;
+
+	logger.scope('MQTT').log(`[RX] ${JSON.stringify(JSON.parse(msg))}`);
+
+	const cmd: IdmCommand = JSON.parse(msg) satisfies IdmCommand;
+	await idm.handleCommand(cmd);
+
+	logger.scope('MQTT').info('Reset command');
+	mqttClient.publish(TOPICS.MQTT.CMD, '');
+});
+
+mqttClient.subscribe(TOPICS.MQTT.CMD);
+//#endregion
